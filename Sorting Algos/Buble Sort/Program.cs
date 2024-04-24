abeb720949b7058b0a0d008f1a80f88b58af6d61
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-04-24 17:24:31 +0300

    added bubble sort implementation

---
+++ Sorting Algos/Buble Sort/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int[] numbers = { 7,6,5,4,3,1,2};
+
+int[] sortedArray = BubleSort(numbers);
+
+Console.WriteLine(String.Join(" ", sortedArray));
+
+static int[] BubleSort(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		bool isSorted = true;
+
+		for (int j = 1; j < array.Length - i; j++)
+		{
+			if (array[j] < array[j - 1])
+			{
+				int temp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = temp;
+				isSorted = false;
+			}
+		}
+
+		if (isSorted)
+		{
+			return array;
+		}
+	}
+
+	return array;
+}
