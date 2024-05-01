e25be3b0af52c3650867d9dedc2ed249383ca916
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-05-01 17:13:41 +0300

    added reverse queue implementation

---
+++ DataStructures/QueueReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿Queue<int> queue = new Queue<int>();
+queue.Enqueue(10);
+queue.Enqueue(20);
+queue.Enqueue(30);
+
+Reverse(queue);
+Console.WriteLine(string.Join(" ", queue));
+
+static void Reverse(Queue<int> queue)
+{
+	Stack<int> stack = new Stack<int>();
+
+	while (queue.Count > 0)
+	{
+		stack.Push(queue.Dequeue());
+	}
+
+	while (stack.Count > 0)
+	{
+		queue.Enqueue(stack.Pop());
+	}
+}
---
+++ Sorting Algos/QuickSort/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] numbers = { 11, 17, 5, 4, 16, 9, 2, 20 };
+
+QuickSort(numbers);
+
+Console.WriteLine(String.Join(" ", numbers));
+
+static void QuickSort(int[] array)
+{
+	
+
+}
+
+static void Partition(int partition)
+{
+	
+}
