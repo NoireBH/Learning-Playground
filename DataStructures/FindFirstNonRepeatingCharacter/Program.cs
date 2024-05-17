4cb6e5723a3857bb137371425af642775f805f58
Elmir Mustafov <114015749+NoireBH@users.noreply.github.com>
2024-05-17 17:51:02 +0300

    added FindFirstNonRepeatingCharacter exercise

---
+++ DataStructures/FindFirstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,38 @@
+﻿string sentence = "the impostor is sus";
+
+string character = FindFirstNonRepeatingCharacter(sentence);
+Console.WriteLine(character);
+
+static string FindFirstNonRepeatingCharacter(string sentence)
+{
+	var dictionary = new Dictionary<char, int>();
+	string firstChar = string.Empty;
+
+	foreach (var character in sentence)
+	{
+		if (Char.IsWhiteSpace(character))
+		{
+			continue;
+		}
+
+		if (!dictionary.ContainsKey(character))
+		{
+			dictionary[character] = 0;
+		}
+
+		dictionary[character]++;		
+	}
+
+	foreach (KeyValuePair<char, int> entry in dictionary)
+	{
+		if (dictionary[entry.Key] == 1)
+		{
+			firstChar = entry.Key.ToString();
+			return firstChar;
+		}
+		
+	}
+
+	return firstChar;
+
+}
